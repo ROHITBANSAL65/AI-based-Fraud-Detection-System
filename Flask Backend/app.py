31e70ca1318759a0f6035e4f35cb0be5860c2baa
ROHIT BANSAL <143306289+ROHITBANSAL65@users.noreply.github.com>
2025-07-17 06:47:34 -0700

    Update app.py

--- Flask Backend/app.py
+++ Flask Backend/app.py
@@ -18,7 +18,7 @@ database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:#Aanchal4@localhost:5432/fraud_detection_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
